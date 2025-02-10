23345ab82fa060eadb080c45edd5c34885453e17
Lectorem <79637710149@mail.ru>
2025-02-10 09:40:52 +0300

    fix - OrganizationDto

--- server/src/main/java/ru/fns/server/dto/OrganizationDto.java
+++ server/src/main/java/ru/fns/server/dto/OrganizationDto.java
@@ -3,10 +3,12 @@ package ru.fns.server.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Setter
 @Getter
 public class OrganizationDto {
-    private Long id;
+    private UUID id;
     private String organization_name;
     private String inn;
     private String kpp;
